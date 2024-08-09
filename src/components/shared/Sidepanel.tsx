b9223846684200a051ed81776a5d0e0d1067a594
raghavawasthi240 <raghavawasthi240@gmail.com>
2024-08-09 23:10:57 +0530

    feat: added sidepanel

---
+++ src/components/shared/Sidepanel.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import { IoMdHome } from "react-icons/io";
+import { FaUserShield } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { IoIosSend } from "react-icons/io";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { FaInbox } from "react-icons/fa";
+import { IoMdStats } from "react-icons/io";
+
+export default function Sidepanel() {
+  return (
+    <div className="w-12 h-screen bg-red-300 flex flex-col items-center py-2 px-1">
+      <Image src="/logo.png" alt="logo" width={26} height={26} />
+
+      <div className="flex flex-col items-center gap-6 pt-12">
+        <IoMdHome className="w-5 h-5" />
+        <FaUserShield className="w-5 h-5" />
+        <IoMdMail className="w-5 h-5" />
+        <IoIosSend className="w-5 h-5" />
+        <AiOutlineMenuFold className="w-5 h-5" />
+        <FaInbox className="w-5 h-5" />
+        <IoMdStats className="w-5 h-5" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
